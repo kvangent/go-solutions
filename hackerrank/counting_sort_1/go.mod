af96490dad3b1916df93327bde5fd71a074b9d71
kpvangent <10712294+kvangent@users.noreply.github.com>
2022-06-23 23:25:03 +0000

    feat: add count_sort_1

---
+++ hackerrank/counting_sort_1/go.mod
@@ -0,0 +1,3 @@
+module counting_sort_1
+
+go 1.18
---
+++ hackerrank/counting_sort_1/problem.go
@@ -0,0 +1,84 @@
+// Copyright 2022 Kurtis Van Gent
+//
+// Use of this source code is governed by an MIT-style license that can be found
+// in the LICENSE file or at https://opensource.org/licenses/MIT.
+
+package counting_sort_1
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'countingSort' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+func countingSort(arr []int) []int {
+	var f [100]int
+	for _, i := range arr {
+		f[i]++
+	}
+	return f[:]
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	arrTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	var arr []int
+
+	for i := 0; i < int(n); i++ {
+		arrItem, err := strconv.ParseInt(arrTemp[i], 10, 64)
+		checkError(err)
+		arr = append(arr, int(arrItem))
+	}
+
+	result := countingSort(arr)
+
+	for i, resultItem := range result {
+		fmt.Fprintf(writer, "%d", resultItem)
+
+		if i != len(result)-1 {
+			fmt.Fprintf(writer, " ")
+		}
+	}
+
+	fmt.Fprintf(writer, "\n")
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
